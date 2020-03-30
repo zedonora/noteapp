2e67c1e26364c44a62fffa5c4d9bb35e969a996b
zedonora <timeundtime@gmail.com>
2020-03-31 06:42:19 +0900

    update note resolvers

--- src/resolvers.js
+++ src/resolvers.js
@@ -1,16 +1,19 @@
 import Note from "./models/note";
 export const resolvers = {
-  Query: {
-    async getNote(root, { _id }) {
-      return await Note.findById(_id);
-    },
-    async allNotes() {
-      return await Note.find();
-    }
-  },
-  Mutation: {
-    async createNote(root, { input }) {
-      return await Note.create(input);
-    }
-  }
+	Query: {
+		async getNote(root, { _id }) {
+			return await Note.findById(_id);
+		},
+		async allNotes() {
+			return await Note.find();
+		},
+		async updateBite(root, { _id }) {
+			return await Note.findById(_id).updateBite();
+		}
+	},
+	Mutation: {
+		async createNote(root, { input }) {
+			return await Note.create(input);
+		}
+	}
 };
